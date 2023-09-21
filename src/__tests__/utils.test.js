b9a95d6fcb126d4f64d4b4a6022b5b8c1b6c4fbe
Xavier FACQ <xavier.facq@free.fr>
2023-09-21 14:23:39 +0200

    Fix linter issue

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,19 +1,17 @@
 import { describe, test, expect } from 'vitest'
 import { stringify } from '../utils'
 
-
 describe('Utils', () => {
-    describe('stringify', () => {
-      test('Verify if stringify() works well', () => {
+  describe('stringify', () => {
+    test('Verify if stringify() works well', () => {
+      expect(stringify({})).toBe('')
 
-        expect(stringify({})).toBe('')
-
-        const obj1 = {
-            id: 123456789,
-            name: 'mock_name',
-            description: 'mock_description'
-        };
-        expect(stringify(obj1)).toBe('?id=123456789&name=mock_name&description=mock_description')
-      });
-    });
-});
+      const obj1 = {
+        id: 123456789,
+        name: 'mock_name',
+        description: 'mock_description'
+      }
+      expect(stringify(obj1)).toBe('?id=123456789&name=mock_name&description=mock_description')
+    })
+  })
+})
