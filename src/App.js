d24191c914f236e0f225a00239d8993f96eddbe7
Xavier FACQ <xavier.facq@free.fr>
2023-09-27 10:19:23 +0200

    Standar --fix

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import ErrorBoundary from './ErrorBoundary'
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons'
 
 AccessibilityModule(Highcharts)
-Highcharts.AST.allowedAttributes.push('rel');
+Highcharts.AST.allowedAttributes.push('rel')
 
 const { Header, Content, Sider } = Layout
 
